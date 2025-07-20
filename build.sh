250a07af672aa6a422b6c752d10376ac851fc311
kolcompass <kolcompass@proton.me>
2025-07-20 19:27:17 +1000

    Fix: Use node directly to run react-scripts

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ npm install --legacy-peer-deps
 echo "Creating build directory..."
 mkdir -p build
 
-echo "Setting up node_modules/.bin in PATH..."
-export PATH="$PWD/node_modules/.bin:$PATH"
+echo "Setting up environment..."
+export CI=false
+export NODE_OPTIONS="--max_old_space_size=2048"
 
-echo "Running build..."
-CI=false react-scripts build 
+echo "Running build with node..."
+node ./node_modules/react-scripts/bin/react-scripts.js build 
