bb44b7cef2d36ee3f70ff6bac7394caaa2bd9977
kolcompass <kolcompass@proton.me>
2025-07-20 20:20:05 +1000

    Fix: Downgrade Express to 4.18.2 and simplify routing to fix path-to-regexp error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Handle React routing - send all requests to index.html
-app.get('*', (req, res) => {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
